73283651918711249277fed180ea0ce3f9a255bb
Anushka <108073638+anushka036@users.noreply.github.com>
2023-01-22 20:11:45 +0530

    Add files via upload

---
+++ Professor.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+public class Professor extends Timetable{
+    private ArrayList<String> courses = new ArrayList<String>();
+    void addCourses(String s){
+        this.courses.add(s);
+    }
+
+
+}
